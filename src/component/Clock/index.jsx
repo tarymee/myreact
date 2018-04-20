1efe8de775e3a6da5eabc107ecbbac214076131b
tarymee <tarymee@gmail.com>
2018-04-20 19:00:49 +0800

    p

---
+++ src/component/Clock/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './clock.css'
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { date: new Date() }
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    render() {
+        return (
+            <div className="m-clock">{this.state.date.toLocaleTimeString()}</div>
+        )
+    }
+}
+
+export default Clock
