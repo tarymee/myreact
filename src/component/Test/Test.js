cc817bbadb6149e17c9f4a4a24866bfa7e48678c
tarymee <tarymee@gmail.com>
2018-05-21 19:07:20 +0800

    up

--- src/component/Test/Test.js
+++ src/component/Test/Test.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import './test.css';
-// import { test } from '../../lib/util'
-// import util, { v1, v2 } from '../../lib/util'
-// import * as util from '../../util'
-// import { version, test } from '../../util'
 
-import util, { version, test } from '../../util'
 
 // import fetchRequest from '../../lib/fetchRequest'
 
@@ -35,12 +30,6 @@ class Test extends React.Component {
     }
 
     componentDidMount() {
-        console.log(util)
-        // console.log(util.v1())
-        // console.log(util.v2())
-        console.log(version())
-        // console.log(v2())
-
         // 如何扩展 Create React App 的 Webpack 配置
         // https://zhaozhiming.github.io/blog/2018/01/08/create-react-app-override-webpack-config/
 
