7c41dde3b31b4ef38e9dc6bc4cb7ec402577598c
tarymee <tarymee@gmail.com>
2020-04-14 00:29:47 +0800

    fix(8877): 555

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,45 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional']
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'body-leading-blank': [1, 'always'],
+        'footer-leading-blank': [1, 'always'],
+        'header-max-length': [2, 'always', 72],
+        'scope-case': [2, 'always', 'lower-case'],
+        'scope-enum': [
+            1,
+            'always',
+            [
+                'person',
+                'a',
+                'b'
+            ]
+        ],
+        'subject-case': [
+            2,
+            'never',
+            ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+        ],
+        'subject-empty': [2, 'never'],
+        'subject-full-stop': [2, 'never', '.'],
+        'type-case': [2, 'always', 'lower-case'],
+        'type-empty': [2, 'never'],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'build',
+                'chore',
+                'ci',
+                'docs',
+                'feat',
+                'fix',
+                'improvement',
+                'perf',
+                'refactor',
+                'revert',
+                'style',
+                'test'
+            ]
+        ]
+    }
 };
